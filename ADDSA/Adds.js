09f59af33aa1af870199f315d376fee6d040ae21
Luis A. Bustos Lucas <luisabustoslucas@gmail.com>
2021-02-18 17:45:11 +0100

    prueba

---
+++ ADDSA/Adds.js
@@ -0,0 +1,35 @@
+{
+        author: 'luisabustoslucas@gmail.com',
+        message: 'Iphone 6',
+        description: 'Iphone 6 nuevo. Viene con todos los complementos.',
+        date: '2021-02-17  11:30:34', 
+        image: '',
+        status: 'venta',
+
+    },
+    {
+        author: 'luisabustoslucas@gmail.com',
+        message: 'Ipad Pro',
+        description: 'Ipad Pro sin estrenar.',
+        date: '2021-02-17  11:30:34', 
+        image: '',
+        status: 'venta'
+
+    },
+    {
+        author: 'luisabustoslucas@gmail.com',
+        message: 'Bicicleta de carretera',
+        description: 'talla 55',
+        date: '2021-02-17  11:30:34', 
+        image: '',
+        status: 'venta'
+
+    },
+    {
+        author: 'luisabustoslucas@gmail.com',
+        message: 'Iphone 6',
+        description: 'Iphone 6 nuevo. Viene con todos los complementos.',
+        date: '2021-02-17  11:30:34', 
+        image: '',
+        status: 'venta'
+    }
